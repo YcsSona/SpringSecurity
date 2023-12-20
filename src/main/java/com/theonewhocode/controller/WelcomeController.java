0023088a5af4412a4de346ce86b86a9c28b8aff1
ycssona <ycssona@gmail.com>
2023-12-21 00:07:14 +0530

    Added support of Spring Security

--- src/main/java/com/theonewhocode/controller/WelcomeController.java
+++ src/main/java/com/theonewhocode/controller/WelcomeController.java
@@ -8,6 +8,6 @@ public class WelcomeController {
 
     @GetMapping("/welcome")
     public String sayWelcome() {
-        return "Welcome to Spring application without security";
+        return "Welcome to Spring application with security";
     }
 }
