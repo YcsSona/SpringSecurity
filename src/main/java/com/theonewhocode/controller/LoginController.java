8b59f284f55451717c6bbe040fc11106aa2a44be
ycssona <ycssona@gmail.com>
2023-12-24 22:30:56 +0530

    Adapt BCryptPasswordEncoder as PasswordEncoder

--- src/main/java/com/theonewhocode/controller/LoginController.java
+++ src/main/java/com/theonewhocode/controller/LoginController.java
@@ -5,6 +5,7 @@ import com.theonewhocode.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,12 +16,17 @@ public class LoginController {
     @Autowired
     private CustomerRepository customerRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@RequestBody Customer customer) {
         Customer savedCustomer = null;
         ResponseEntity<?> response = null;
 
         try {
+            String hashedPwd = passwordEncoder.encode(customer.getPwd());
+            customer.setPwd(hashedPwd);
             savedCustomer = customerRepository.save(customer);
 
             if (savedCustomer.getId() > 0) {
