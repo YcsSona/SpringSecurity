df3f685892f8f02cf2bae5bd37a0d46dec3d721b
ycssona <ycssona@gmail.com>
2023-12-24 11:02:10 +0530

    Building a new REST API to allow the registration of new User

--- src/main/java/com/theonewhocode/config/SecurityConfig.java
+++ src/main/java/com/theonewhocode/config/SecurityConfig.java
@@ -3,13 +3,9 @@ package com.theonewhocode.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -18,9 +14,10 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(requests ->
-                requests.requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
-                        .requestMatchers("/notices", "/contact").permitAll());
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(requests ->
+                        requests.requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
+                                .requestMatchers("/notices", "/contact", "/register").permitAll());
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
