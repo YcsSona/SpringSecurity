4ae334cbd193d4bd6716765cd9c41a32f4510100
ycssona <ycssona@gmail.com>
2023-12-25 19:42:37 +0530

    Implementing CSRF token solution

--- src/main/java/com/theonewhocode/config/SecurityConfig.java
+++ src/main/java/com/theonewhocode/config/SecurityConfig.java
@@ -1,12 +1,17 @@
 package com.theonewhocode.config;
 
+import com.theonewhocode.filter.CsrfCookieFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
@@ -19,7 +24,14 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+        // CsrfTokenRequestAttributeHandler : responsible for to create csrf token as request attribute
+        // and pass it in the form of cookie or header
+        CsrfTokenRequestAttributeHandler requestAttributeHandler = new CsrfTokenRequestAttributeHandler();
+        requestAttributeHandler.setCsrfRequestAttributeName("_csrf");
+
+        http.securityContext(context -> context.requireExplicitSave(false))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+                .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
                     @Override
                     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
                         CorsConfiguration config = new CorsConfiguration();
@@ -31,7 +43,12 @@ public class SecurityConfig {
                         return config;
                     }
                 }))
-                .csrf(csrf -> csrf.ignoringRequestMatchers("/contact", "/register"))
+                .csrf(csrf -> csrf.csrfTokenRequestHandler(requestAttributeHandler)
+                        .ignoringRequestMatchers("/contact", "/register")
+                        // CookieCsrfTokenRepository : persists the token in cookie
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+                // invoke CsrfCookieFilter() after BasicAuthenticationFilter
+                .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
                 .authorizeHttpRequests(requests ->
                         requests.requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards", "/user").authenticated()
                                 .requestMatchers("/notices", "/contact", "/register").permitAll());
