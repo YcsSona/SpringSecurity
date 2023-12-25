58605b588e81e8457ebd46f071730cca6c2e294e
ycssona <ycssona@gmail.com>
2023-12-25 18:30:19 +0530

    Fixing CORs issue globally

--- src/main/java/com/theonewhocode/config/SecurityConfig.java
+++ src/main/java/com/theonewhocode/config/SecurityConfig.java
@@ -1,11 +1,16 @@
 package com.theonewhocode.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -14,9 +19,21 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())
+        http.cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                        CorsConfiguration config = new CorsConfiguration();
+                        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+                        config.setAllowedMethods(Collections.singletonList("*"));
+                        config.setAllowCredentials(true);
+                        config.setAllowedHeaders(Collections.singletonList("*"));
+                        config.setMaxAge(3600L);
+                        return config;
+                    }
+                }))
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(requests ->
-                        requests.requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards","/user").authenticated()
+                        requests.requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards", "/user").authenticated()
                                 .requestMatchers("/notices", "/contact", "/register").permitAll());
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
